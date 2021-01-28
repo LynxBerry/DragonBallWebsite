8e72666baa541e12fb39546ef5209a2592a68a76
Steven Shao <stevenshao2016@icloud.com>
2021-01-28 11:34:37 +0800

    update change view

--- scripts/AppViewModel.js
+++ scripts/AppViewModel.js
@@ -1,7 +1,58 @@
 define(["knockout"], function(ko) {
+
+    function View1(countDown)
+    {
+        this.name = "view1";
+        this.data = { 
+            name: "loading...",
+            number: countDown
+
+        };
+    }
+
+    function View2()
+    {
+        this.name = "view2";
+        this.data ={ fruit: ["grape", "waterlemon", "banana"]};
+    }
+
+
     return function AppViewModel() {
         this.firstName = ko.observable("Zteven");
         this.lastName = ko.observable("Shao");
+        this.totalSeconds = 20;
+        this.countDown = ko.observable(this.totalSeconds);
+        this.View1 = new View1(this.countDown); // laoding view
+        this.View2 = new View2();
+
+       
+        this.isStillLoading = ko.observable(true);
+        // this.currentView = ko.observable(this.View1);
+        var self = this;
+        this.currentView = ko.pureComputed(function() {
+            if (self.isStillLoading() === true) {
+                return self.View1;
+            }
+
+            return self.View2;
+
+        });
+
+        this.init = function __init(){
+            var self = this;
+            var countDown = function () {
+                setTimeout(() => {
+                    if (self.countDown() === 0) {
+                        self.isStillLoading(false);
+                    } else {
+                        self.countDown(self.countDown() - 1)
+                        countDown();
+                    }     
+                }, 1000);
+            };
+
+            countDown();
+        };
 
         this.fullName = ko.computed(function() {
             return this.firstName() + " " + this.lastName();
@@ -10,6 +61,14 @@ define(["knockout"], function(ko) {
         this.capitalizeLastName = function() {
             var currentVal = this.lastName();
             this.lastName(currentVal.toUpperCase());
+        };
+
+        this.changeView = function() {
+            if (this.currentView().name === "view1")
+                this.currentView(this.View2);
+            else
+                this.currentView(this.View1);
+
         };
     }
 
--- scripts/Main.js
+++ scripts/Main.js
@@ -1,8 +1,9 @@
 define(["jquery","knockout","AppViewModel"], function ($, ko, AppViewModel) {
     $(document).ready(function myfunc() {
         // window.alert("The JQuery Version is " +  $().jquery + ".");
-        
-        ko.applyBindings(new AppViewModel());
+        var vm = new AppViewModel();
+        ko.applyBindings(vm);
+        vm.init();
 
     });
     
